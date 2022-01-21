4c9b49c398de34a7c4f700a75d2c63f7ea5e3822
dorjoo <dorjoohover@gmail.com>
2022-01-21 19:44:43 +0800

    adf

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const pages = document.getElementsByClassName('img')
-
+for(let i = 0; i <pages.length - 1;i ++) {
+     if(i%2 ===0 ){
+          pages[i].style.zIndex = pages.length - i
+     }
+     console.log(pages[i].style.zIndex + i)
+}
 document.addEventListener('DOMContentLoaded', function() {
-     for(var i = 0; i < pages.length -1;  i++) {
-          if(i%2 === 0 ) {
-               pages[i].style.zIndex = (pages.length - i)
-          }
+     for(let i = 0; i < pages.length -1;  i++) {
+          
           pages[i].pageNum = i + 1
           pages[i].onclick = function(){
+               
                if(this.pageNum % 2 === 0) {
                     this.classList.remove('flipped')
                     this.previousElementSibling.classList.remove('flipped')
                } else {
-                    pages[i].style.zIndex
+                   
+
+                    // console.log( pages[this.pageNum].style.zIndex)
                     this.classList.add('flipped')
                     this.nextElementSibling.classList.add('flipped')
-
                }
           } 
      }
